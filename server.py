18351e50100e76fec57aef66a6c7c0237d0f98a0
Phạm Nhật Quang <pnquang05@gmail.com>
2023-09-18 03:15:23 +0700

    Restructuring, SocketIO support

--- server.py
+++ server.py
@@ -1,10 +1,17 @@
 import flask
+from flask_cors import CORS
+from flask_socketio import SocketIO
 
 app = flask.Flask(__name__)
+sio = SocketIO(app, cors_allowed_origins='*')
+
+CORS(app)
+import wsevent_handler
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return "Hi THT i'm alive so pls give me Điểm rèn luyện or else i'll not function as intended pls"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    sio.run(app, port=8000, debug=True)
+
---
+++ wsevent_handler.py
@@ -0,0 +1,18 @@
+from flask import request
+from flask_socketio import emit
+try:
+    from __main__ import sio
+except ImportError:
+    from server import sio
+
+def logInfo(msg):
+    print(f"[sid={request.sid}]: {msg}.")
+
+@sio.on("connect")
+def onConnect():
+    emit("get-join", "connected")
+    logInfo("connected, test data sent")
+
+@sio.on("disconnect")
+def onDisconnect():
+    logInfo("disconnected")
